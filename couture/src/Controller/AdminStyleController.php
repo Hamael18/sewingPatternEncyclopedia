5ca8619fb6df4d50ad42223e21e7383108fd98b7
TinouWild <tinouclt@gmail.com>
2019-07-14 10:43:57 +0200

    CRUD

--- couture/src/Controller/AdminStyleController.php
+++ couture/src/Controller/AdminStyleController.php
@@ -2,18 +2,80 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Style;
+use App\Form\StyleType;
+use App\Repository\StyleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminStyleController extends AbstractController
+class AdminStyleController extends BaseController
 {
     /**
      * @Route("/admin/style", name="admin_style")
      */
-    public function index()
+    public function index(StyleRepository $repository)
     {
-        return $this->render('admin_style/index.html.twig', [
-            'controller_name' => 'AdminStyleController',
+        return $this->render('admin/style/index.html.twig', [
+            'styles' => $repository->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/admin/style/new", name="admin_style_new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newStyle(Request $request)
+    {
+        $style = new Style();
+        $form = $this->createForm(StyleType::class, $style);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($style);
+            $this->manager->flush();
+            $this->addFlash('success', "Style ajouté avec succès !");
+            return $this->redirectToRoute('admin_style');
+        }
+
+        return $this->render('admin/style/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/style/edit/{id}", name="admin_style_edit")
+     * @param Request $request
+     * @param Style $style
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editStyle(Request $request, Style $style)
+    {
+        $form = $this->createForm(StyleType::class, $style);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+            $this->addFlash('success', "Style modifié avec succès !");
+            return $this->redirectToRoute('admin_style');
+        }
+
+        return $this->render('admin/style/edit.html.twig', [
+            'form' => $form->createView(),
+            'style' => $style
+        ]);
+    }
+
+    /**
+     * @Route("/admin/style/delete/{id}", name="admin_style_delete")
+     * @param Style $style
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteStyle(Style $style)
+    {
+        $this->manager->remove($style);
+        $this->manager->flush();
+        $this->addFlash('success', "Style supprimé avec succès !");
+        return $this->redirectToRoute('admin_style');
     }
 }
