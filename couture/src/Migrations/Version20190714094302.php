04a1aafc2915f059dc16c12fab5e4c3d4d65c8f1
TinouWild <tinouclt@gmail.com>
2019-07-14 11:51:53 +0200

    correction migration

--- couture/src/Migrations/Version20190714094302.php
+++ couture/src/Migrations/Version20190714094302.php
@@ -23,7 +23,6 @@ final class Version20190714094302 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE handle (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE collar');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20190714094302 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE collar (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE handle');
     }
 }
