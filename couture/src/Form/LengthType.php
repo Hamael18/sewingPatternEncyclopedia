449e61372ecac340e5aee705fccd524829336677
TinouWild <tinouclt@gmail.com>
2019-07-14 12:38:33 +0200

    mise en form des formulaires

--- couture/src/Form/LengthType.php
+++ couture/src/Form/LengthType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Length;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class LengthType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Libellé de la longueur']
+            ])
         ;
     }
 
