bcb754529bbf77ae6d4886be6daf497a9f2ee6d0
TinouWild <tinouclt@gmail.com>
2019-07-14 12:55:21 +0200

    CRUD length

---
+++ couture/src/Controller/AdminLengthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Length;
+use App\Form\LengthType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminLengthController extends BaseController
+{
+    /**
+     * @Route("/admin/length", name="admin_length")
+     */
+    public function index()
+    {
+        return $this->render('admin/length/index.html.twig', [
+            'controller_name' => 'AdminLengthController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/length/new", name="admin_length_new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newCollar(Request $request)
+    {
+        $length = new Length();
+        $form = $this->createForm(LengthType::class, $length);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($length);
+            $this->manager->flush();
+            $this->addFlash('success', "Longueur ajoutée avec succès !");
+            return $this->redirectToRoute('admin_length');
+        }
+
+        return $this->render('admin/length/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/length/edit/{id}", name="admin_length_edit")
+     * @param Request $request
+     * @param Length $length
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editStyle(Request $request, Length $length)
+    {
+        $form = $this->createForm(LengthType::class, $length);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+            $this->addFlash('success', "Longueur modifiée avec succès !");
+            return $this->redirectToRoute('admin_length');
+        }
+
+        return $this->render('admin/length/edit.html.twig', [
+            'form' => $form->createView(),
+            'style' => $length
+        ]);
+    }
+
+    /**
+     * @Route("/admin/length/delete/{id}", name="admin_length_delete")
+     * @param Length $length
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteStyle(Length $length)
+    {
+        $this->manager->remove($length);
+        $this->manager->flush();
+        $this->addFlash('success', "Longueur supprimée avec succès !");
+        return $this->redirectToRoute('admin_length');
+    }
+}
