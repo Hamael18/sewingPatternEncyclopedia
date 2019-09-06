1742e402c0711420b8bb90d260d3e457bb204a71
TinouWild <tinouclt@gmail.com>
2019-09-06 15:49:27 +0200

    DRY pagination

--- couture/src/Service/FilterObjectsBrand.php
+++ couture/src/Service/FilterObjectsBrand.php
@@ -5,11 +5,43 @@ namespace App\Service;
 
 
 use App\Entity\Brand;
+use App\Entity\Pattern;
 use App\Entity\User;
+use PHPUnit\Runner\Exception;
+use Symfony\Component\Security\Core\Security;
 
 class FilterObjectsBrand
 {
-    public function getIdsBrand($user)
+    private $entityClass;
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function getFilterList()
+    {
+        if ($this->entityClass == Brand::class) {
+            return $this->getFilterForBrand();
+        } elseif ($this->entityClass == Pattern::class) {
+            return $this->getFilterForPattern($this->security->getUser());
+        }
+
+        return new Exception();
+    }
+
+    public function getFilterForBrand()
+    {
+        return ['owner' => $this->security->getUser()];
+    }
+
+    /**
+     * @param $user
+     *
+     * @return array
+     */
+    public function getFilterForPattern($user)
     {
         /** @var User $user $idBrandsOfUser */
         $idBrandsOfUser = [];
@@ -18,5 +50,17 @@ class FilterObjectsBrand
             $idBrandsOfUser['brand'][] = $brand->getId();
         }
         return $idBrandsOfUser;
+    }
+
+    public function getEntityClass()
+    {
+        return $this->entityClass;
+    }
+
+    public function setEntityClass($entityClass)
+    {
+        $this->entityClass = $entityClass;
+
+        return $this;
     }
 }
