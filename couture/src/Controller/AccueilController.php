19d0d53e593f3b087dd6a9cfba85396ebbb7568e
Hamael18 <hamael18@hotmail.com>
2019-10-01 09:51:10 +0200

    Show newly added patterns

--- couture/src/Controller/AccueilController.php
+++ couture/src/Controller/AccueilController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PatternRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil")
+     * @param PatternRepository $patternRepo
+     *
+     * @return Response
      */
-    public function index()
+    public function index(PatternRepository $patternRepo)
     {
         return $this->render('front_office/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'lastPatterns' => $patternRepo->findBy([], ['createdAt' => 'DESC'], 5),
         ]);
     }
 }
